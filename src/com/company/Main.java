e136a6ed512095c3c470233d18b09483782afc1a
BT_1N3_24@bktech.local <BT_1N3_24@bktech.local>
2018-10-17 08:24:23 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+public abstract class Shape
+{
+    private String type;
+    public Shape(String type)
+    {
+        this.type = 
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
